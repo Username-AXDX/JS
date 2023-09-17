592e4e4c54b0a81958edc30499569f4788991e13
Username-AXDX <ilovesmal63@gmail.com>
2023-09-17 15:23:43 +0400

    remove  folder

--- test.js
+++ test.js
@@ -1,11 +1,5 @@
 const fs = require('fs');
 const my_modul = require('./my_modul');
-fs.writeFileSync('some.txt', 'Hello world!');
-let result = fs.readFileSync('some.txt', 'utf-8');
-console.log(result);
-fs.mkdir('new_file', () => {
-  fs.writeFile('./new_file/lm.txt', 'This is a good one', () => {});
-});
 fs.unlink('./new_file/lm.txt', () => {
-  fs.rmdir('./lm.txt'), () => {};
+  fs.rmdir('./new_file'), () => {};
 });
