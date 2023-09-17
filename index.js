0e7fe3d21a62ac2dd048dbbd2334cb99f9b4ee1b
Username-AXDX <ilovesmal63@gmail.com>
2023-09-17 12:28:04 +0400

    second

--- index.js
+++
@@ -1,1 +0,0 @@
-console.log("Sacha lox");
---
+++ test.js
@@ -0,0 +1,7 @@
+const CC = require('currency-converter-lt');
+let currencyConverter = new CC({from: 'USD', to: 'RUB', amount: 1});
+currencyConverter;
+currencyConverter.convert().then((response) => {
+  console.log(response);
+});
+let a = 5;
