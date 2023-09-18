2167c70e109fedee857c972676742faa23f6b9fa
Username-AXDX <ilovesmal63@gmail.com>
2023-09-18 18:50:00 +0400

    Even ore Odd?

--- Pillars.js
+++
@@ -1,7 +0,0 @@
-function pillars(numPill, dist, width) {
-  let L = 0;
-  numPill > 1 ? (L = (numPill - 2) * width + 100 * dist * (numPill - 1)) : L;
-
-  return L;
-}
-//https://www.codewars.com/kata/5bb0c58f484fcd170700063d/train/javascript
--- test.js
+++ test.js
@@ -1,5 +1,10 @@
-const fs = require('fs');
-const my_modul = require('./my_modul');
-fs.unlink('./new_file/lm.txt', () => {
-  fs.rmdir('./new_file'), () => {};
+const http = require('http');
+let server = http.createServer((req, res) => {
+  rec.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+  rec.end('Hello JS');
+});
+const PORT = 3000;
+const HOST = 'localhost';
+server.listen(PORT, HOST, () => {
+  console.log(`Сервер запущен : http://${HOST}:${PORT}`);
 });
