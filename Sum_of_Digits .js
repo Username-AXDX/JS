df02c25ba46167535cb6a1bc9ff3763f931eca25
Username-AXDX <ilovesmal63@gmail.com>
2023-09-28 16:21:30 +0400

    Sum of Digits v:2

--- Sum_of_Digits .js
+++ Sum_of_Digits .js
@@ -1,19 +1,23 @@
 function digitalRoot(n) {
-  function sum_of_digits(N = n) {
-    let array = [];
-    for (let i = 0; b !== 0; i++) {
-      array[i] = N % 10;
-      N = parseInt(N, 10);
-    }
-    let sum = 0;
-    for (let i of array) {
-      sum += i;
-    }
-    return sum;
-  }
-  while (n % 10 !== 0) {
-    n = sum_of_digits(n);
-  }
-  return n;
+  let value = 0;
+  do {
+    value = sum_of_digits(n);
+  } while (value % 10 !== 0);
+
+  return value;
 }
+
+function sum_of_digits(N) {
+  let array = [];
+  let b = N;
+  for (let i = 0; b !== 0; i++) {
+    array[i] = N % 10;
+    b = parseInt(b, 10);
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+} //partucalar function for adding digits
 //https://www.codewars.com/kata/541c8630095125aba6000c00/train/javascript
