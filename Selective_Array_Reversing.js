f41c8fb9ab0793c921ca85e6e9218303abbffa5a
Username-AXDX <ilovesmal63@gmail.com>
2023-10-06 20:35:14 +0400

    I get it

--- Selective_Array_Reversing.js
+++ Selective_Array_Reversing.js
@@ -6,11 +6,14 @@ function selReverse(array, length) {
   if (array.length < length) {
     return array.reverse();
   }
-  for (i = length - 1; array.length !== 0; ) {
-    new_array = Array.of(array.slice(0, i).reverse());
+  let new_i = 0;
+  for (i = length; array.length !== 0; ) {
+    new_array[new_i] = Array.from(array.slice(0, i).reverse());
     array.splice(0, i);
+    new_i++;
   }
-  return new_array;
+  return new_array.flat();
 }
+console.log(selReverse([1, 2, 3, 4, 5, 6], 2));
 
 //https://www.codewars.com/kata/58f6000bc0ec6451960000fd
