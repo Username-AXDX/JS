10180f60c6589bae5555d7bfd98d49127361bd0b
Username-AXDX <ilovesmal63@gmail.com>
2023-10-18 15:16:28 +0400

    the first Unit test

--- test.js
+++
@@ -1,9 +0,0 @@
-const PROPERTY_NAME = 'p1';
-function computePropertyName() {
-  return 'p' + 2;
-}
-let p = {
-  [PROPERTY_NAME]: 1,
-  [computePropertyName]: 2
-};
-console.log(p.p1 + p.p2);
---
+++ test/first_tests.js
@@ -0,0 +1,15 @@
+const assert = require('chai').assert;
+const pow = require('../pow');
+describe('pow', function () {
+  describe('возводит x в степень 3', function () {
+    function makeTest(x) {
+      let expected = x * x * x;
+      it(`${x} в степени 3 будет ${expected}`, function () {
+        assert.equal(pow(x, 3), expected);
+      });
+    }
+    for (let x = 1; x <= 5; x++) {
+      makeTest(x);
+    }
+  });
+});
